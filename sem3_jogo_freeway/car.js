bf316b0d02eb17782f139817f118b3985c62bd4c
Arnaldo Neto <costanetoow@gmail.com>
2022-02-05 21:17:30 -0300

    Criando mais carros

--- sem3_jogo_freeway/car.js
+++ sem3_jogo_freeway/car.js
@@ -1,15 +1,30 @@
 // Código do carro
 
-// Variáveis do carro
+// Variáveis do carro 1
 let xCar = 600;
 let yCar = 40;
+let carSpeed = 2;
+
+// Variáveis do carro 2
+let xCar2 = 600;
+let yCar2 = 96;
+let carSpeed2 = 2.5;
+
+// Variáveis do carro 3
+let xCar3 = 600;
+let yCar3 = 150;
+let carSpeed3 = 3;
 
 // Exibe o carro com seus devidos parâmetros
 function showCar() {
   image(carImage, xCar, yCar, 50, 40);
+  image(carImage2, xCar2, yCar2, 50, 40);
+  image(carImage3, xCar3, yCar3, 50, 40);
 }
 
 // Movimenta o carro
 function moveCar() {
-  xCar -= 2;
+  xCar -= carSpeed;
+  xCar2 -= carSpeed2;
+  xCar3 -= carSpeed3;
 }
--- sem3_jogo_freeway/images.js
+++ sem3_jogo_freeway/images.js
@@ -4,10 +4,14 @@
 let roadImage;
 let actorImage;
 let carImage;
+let carImage2;
+let carImage3;
 
 // Carregando as imagens no projeto
 function preload() {
   roadImage = loadImage("./images/road.png");
   actorImage = loadImage("./images/actor-1.png");
   carImage = loadImage("./images/car-1.png");
+  carImage2 = loadImage("./images/car-2.png");
+  carImage3 = loadImage("./images/car-3.png");
 }
