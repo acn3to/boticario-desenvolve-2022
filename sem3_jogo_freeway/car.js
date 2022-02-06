a37c1997226425e8618936e3dc0510a1ba3d54d3
Arnaldo Neto <costanetoow@gmail.com>
2022-02-06 18:27:50 -0300

    Usando array

--- sem3_jogo_freeway/car.js
+++ sem3_jogo_freeway/car.js
@@ -1,43 +1,32 @@
 // Código do carro
 
-// Variáveis do carro 1
-let xCar = 600;
-let yCar = 40;
-let carSpeed = 2;
-
-// Variáveis do carro 2
-let xCar2 = 600;
-let yCar2 = 96;
-let carSpeed2 = 2.5;
-
-// Variáveis do carro 3
-let xCar3 = 600;
-let yCar3 = 150;
-let carSpeed3 = 3;
+let yCars = [40, 96, 150];
+let carSpeeds = [2, 2.2, 3];
+let xCars = [600, 600, 600];
 
 // Exibe o carro com seus devidos parâmetros
 function showCar() {
-  image(carImage, xCar, yCar, 50, 40);
-  image(carImage2, xCar2, yCar2, 50, 40);
-  image(carImage3, xCar3, yCar3, 50, 40);
+  image(carImage, xCars[0], yCars[0], 50, 40);
+  image(carImage2, xCars[1], yCars[1], 50, 40);
+  image(carImage3, xCars[2], yCars[2], 50, 40);
 }
 
 // Movimenta o carro
 function moveCar() {
-  xCar -= carSpeed;
-  xCar2 -= carSpeed2;
-  xCar3 -= carSpeed3;
+  xCars[0] -= carSpeeds[0];
+  xCars[1] -= carSpeeds[1];
+  xCars[2] -= carSpeeds[2];
 }
 
 // Retorna o carro para posição inicial
 function returnInitialPositionCar() {
-  if (xCar < -50) {
-    xCar = 600;
+  if (xCars[0] < -50) {
+    xCars[0] = 600;
   }
-  if (xCar2 < -50) {
-    xCar2 = 600;
+  if (xCars[1] < -50) {
+    xCars[1] = 600;
   }
-  if (xCar3 < -50) {
-    xCar3 = 600;
+  if (xCars[2] < -50) {
+    xCars[2] = 600;
   }
 }
