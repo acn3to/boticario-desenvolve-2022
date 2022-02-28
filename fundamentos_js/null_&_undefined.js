eca9f8d481cbb9b5b3c3bdfd6acf8832f866c582
Arnaldo Neto <costanetoow@gmail.com>
2022-02-28 20:37:49 -0300

    Tipos null e undefined

---
+++ fundamentos_js/null_&_undefined.js
@@ -0,0 +1,15 @@
+// Tipos null e undefined
+
+let input = null;
+
+if (input === null) {
+  console.log("não há informação");
+} else {
+  console.log(input);
+}
+
+let input2 = null;
+let input3;
+
+console.log(input2); // null
+console.log(input3); // undefined
