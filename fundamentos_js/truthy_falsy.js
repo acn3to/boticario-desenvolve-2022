3bfcce3718bc0797cfbab5659239c0ab626a039b
Arnaldo Neto <costanetoow@gmail.com>
2022-02-28 21:52:27 -0300

    Truthy e Falsy

---
+++ fundamentos_js/truthy_falsy.js
@@ -0,0 +1,14 @@
+// Tipo boolean
+
+const userLoggedIn = true;
+const billPaid = false;
+
+// Truthy ou falsy
+
+// 0 = false
+// 1 = true
+
+console.log(0 == false); // True
+console.log("" == false); // True
+console.log(1 == false); // False
+console.log("a" == false); // False
