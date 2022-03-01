c54fcb1e85db95020e20b79194d28383b6d03bd9
Arnaldo Neto <costanetoow@gmail.com>
2022-03-01 10:09:36 -0300

    Operadores de comparação

---
+++ fundamentos_js/comparacoes.js
@@ -0,0 +1,13 @@
+// Operadores de comparação
+
+const number = 5;
+const text = "5";
+
+console.log(number == text);
+console.log(number === text);
+
+console.log(typeof number);
+console.log(typeof text);
+
+// == Compara apenas o valor
+// === Compara o valor e o tipo de dado
