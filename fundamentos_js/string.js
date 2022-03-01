9223d15f6eb159853c3bc8fb5ac3092238dd4032
Arnaldo Neto <costanetoow@gmail.com>
2022-03-01 10:25:03 -0300

    Template string com operador ternário

--- fundamentos_js/string.js
+++ fundamentos_js/string.js
@@ -16,3 +16,15 @@ var a = 5;
 var b = 10;
 
 console.log(`Quinze é ${a + b} e não ${2 * a + b}.`);
+
+const client = "Neto";
+const age = 28;
+const drinkOver18 = "beer";
+const drinkUnder18 = "juice";
+
+// Template string com operador ternário
+
+const order = `${client} says: "please, wanna drink some ${
+  age >= 18 ? drinkOver18 : drinkUnder18
+}."`;
+console.log(order);
