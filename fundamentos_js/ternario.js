5b0d9bc45d883c261098180223272427da8b7069
Arnaldo Neto <costanetoow@gmail.com>
2022-03-01 10:16:57 -0300

    Operadores ternários

---
+++ fundamentos_js/ternario.js
@@ -0,0 +1,15 @@
+// Operadores ternários
+
+const minAge = 18;
+const clientAge = 19;
+
+// if (clientAge >= minAge) {
+//   console.log("Here is your beer!");
+// } else {
+//   console.log("Just juice for you...");
+// }
+
+console.log(
+  //Condição            //True                 //False
+  clientAge >= minAge ? "Here is your beer!" : "Just juice for you..."
+);
