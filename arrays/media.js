ca19a7b345c71d44da6a4f7f42728679590a3013
Arnaldo Neto <costanetoow@gmail.com>
2022-03-03 12:47:42 -0300

    Arrays

---
+++ arrays/media.js
@@ -0,0 +1,7 @@
+// Calculando médias
+
+let scores = [10, 6.5, 8, 7.5]; // Criando um array
+
+let average = (scores[0] + scores[1] + scores[2] + scores[3]) / scores.length;
+
+console.log(average);
