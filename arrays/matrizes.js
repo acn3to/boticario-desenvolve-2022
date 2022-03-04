ca157e4cdf19dbb812abe0e3b406d07a8d3c6fb8
Arnaldo Neto <costanetoow@gmail.com>
2022-03-04 13:55:43 -0300

    Matrizes

---
+++ arrays/matrizes.js
@@ -0,0 +1,10 @@
+// Matrizes - Arrays de duas dimensões
+
+const students = ["Ana", "Neto", "Lucas", "Igor"];
+const scores = [10, 8, 2, 7];
+
+const studentsAndScores = [students, scores];
+
+console.log(
+  `${studentsAndScores[0][1]}, your score is ${studentsAndScores[1][1]}`
+);
