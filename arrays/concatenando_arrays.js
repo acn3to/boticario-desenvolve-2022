56cfe19f8818b4463520eddbf09e4b6fb63aa80e
Arnaldo Neto <costanetoow@gmail.com>
2022-03-04 12:46:03 -0300

    Concatenando Arrays / Método concat

---
+++ arrays/concatenando_arrays.js
@@ -0,0 +1,27 @@
+// Concatenando Arrays ()
+
+const classA = [
+  "Arnaldo",
+  "Joao",
+  "Lucas",
+  "Rodrigo",
+  "Paula",
+  "Marcelo",
+  "Renata",
+  "Fabiano",
+];
+
+const classB = [
+  "Pedro",
+  "Yan",
+  "Vitor",
+  "Fabiola",
+  "Paulo",
+  "Edvaldo",
+  "Igor",
+  "Carlos",
+];
+
+const classC = classA.concat(classB);
+
+console.log(classC);
