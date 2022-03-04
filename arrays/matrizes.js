12e67cd4defc278aa263bdecde21e07f8bcb45bf
Arnaldo Neto <costanetoow@gmail.com>
2022-03-04 14:02:12 -0300

    Matrizes / Arrays de duas dimensões

--- arrays/matrizes.js
+++ arrays/matrizes.js
@@ -8,3 +8,13 @@ const studentsAndScores = [students, scores];
 console.log(
   `${studentsAndScores[0][1]}, your score is ${studentsAndScores[1][1]}`
 );
+
+const ages = [30, 35, 28];
+const names = ["Ana", "Juliana", "Leonardo"];
+const college = [false, true, true];
+
+const employees = [names, ages, college];
+
+console.log(
+  `${employees[0][0]}, your age is ${employees[1][0]} and you are on college = ${employees[2][0]}`
+);
