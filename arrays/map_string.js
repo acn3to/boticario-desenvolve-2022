b04c11909d7dd045a7712d66d7139458056263f9
Arnaldo Neto <costanetoow@gmail.com>
2022-03-04 16:51:42 -0300

    Atualizando strings / Método map

---
+++ arrays/map_string.js
@@ -0,0 +1,7 @@
+// Atualizando strings com map
+
+const names = ["Arnaldo Neto", "Joao PEDRO", "Lucas REIS", "Ana Paula"];
+
+const namesUpdated = names.map((name) => name.toUpperCase());
+
+console.log(namesUpdated);
