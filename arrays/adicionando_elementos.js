3e6541b31f5e991c94835ba44c44ba32d476e708
Arnaldo Neto <costanetoow@gmail.com>
2022-03-04 08:40:26 -0300

    Adicionando elementos / Método push

---
+++ arrays/adicionando_elementos.js
@@ -0,0 +1,12 @@
+// Adicionando elementos (.push)
+
+let scores = [10, 6.5, 8, 7.5];
+
+scores.push(9);
+
+console.log(scores);
+
+let average =
+  (scores[0] + scores[1] + scores[2] + scores[3] + scores[4]) / scores.length;
+
+console.log(average);
