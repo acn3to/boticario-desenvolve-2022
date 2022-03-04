3ddd4a77a4cf66ad18ec78911f92a925506e911d
Arnaldo Neto <costanetoow@gmail.com>
2022-03-04 16:30:09 -0300

    Callback functions

---
+++ arrays/callbacks.js
@@ -0,0 +1,18 @@
+// Callback - Funções que chamam funções
+
+const names = [
+  "Arnaldo",
+  "Joao",
+  "Lucas",
+  "Rodrigo",
+  "Paula",
+  "Marcelo",
+  "Renata",
+  "Fabiano",
+];
+
+names.forEach(showNames);
+
+function showNames(name) {
+  console.log(name);
+}
