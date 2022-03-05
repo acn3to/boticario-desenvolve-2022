6c7e50cbdd0d5b6064aeaa5ecb240a4b35a78e78
Arnaldo Neto <costanetoow@gmail.com>
2022-03-05 16:32:53 -0300

    Acessando valores de um objeto

---
+++ objetos_js/acessando_valores.js
@@ -0,0 +1,15 @@
+// Acessando valores de um objeto e aplicando métodos JavaScript
+
+const client = {
+  name: "Arnaldo Costa Neto",
+  age: 28,
+  cpf: "85555547778",
+  email: "costa@gmail.com",
+};
+
+console.log(
+  `The client name is ${client.name}
+His age is ${client.age}
+The 3 first numbers of CPF is ${client.cpf.substring(0, 3)}
+His email is ${client.email}`
+);
