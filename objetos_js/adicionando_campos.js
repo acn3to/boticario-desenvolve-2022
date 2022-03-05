36321d9719a09b85ae4fcd270772ba13e74edd41
Arnaldo Neto <costanetoow@gmail.com>
2022-03-05 16:59:20 -0300

    Adicionando e substituindo valores de um objeto

---
+++ objetos_js/adicionando_campos.js
@@ -0,0 +1,13 @@
+// Adicionando campos ao objeto
+
+const client = {
+  name: "Arnaldo Costa Neto",
+  age: 28,
+  cpf: "85555547778",
+  email: "costa@gmail.com",
+};
+
+client.phone = "5571999999999"; // Adiciona um campo ao objeto
+client.cpf = "99999999999"; // Substitui o atual valor
+
+console.log(client);
