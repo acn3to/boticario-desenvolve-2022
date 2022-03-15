fe46661c90716b27172ea08a84de93ab8033a8f8
Arnaldo Neto <costanetoow@gmail.com>
2022-03-15 09:42:26 -0300

    (Descontinuado) Adicionando body-parser e criando uma nova rota POST

--- node_rest/controllers/character.js
+++ node_rest/controllers/character.js
@@ -2,4 +2,9 @@ module.exports = (app) => {
   app.get("/character", (req, res) =>
     res.send("You are on the character route and using a GET request")
   );
+
+  app.post("/character", (req, res) => {
+    console.log(req.body);
+    res.send("You are on the character route and using a POST request");
+  });
 };
