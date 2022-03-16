b1fc9d11180d60b18cdcf6e976730721cb1ae058
Arnaldo Neto <costanetoow@gmail.com>
2022-03-16 18:48:31 -0300

    (Descontinuado) Criando tabela characters

---
+++ node_rest/infra/tables.js
@@ -0,0 +1,22 @@
+class Tables {
+  init(connection) {
+    this.connection = connection;
+
+    this.createCharacter();
+  }
+
+  createCharacter() {
+    const sql =
+      "CREATE TABLE Characters (id int NOT NULL AUTO_INCREMENT, name varchar(50) NOT NULL, race varchar(20), gender varchar(10), PRIMARY KEY (id))";
+
+    this.connection.query(sql, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("Table character created successfully");
+      }
+    });
+  }
+}
+
+module.exports = new Tables();
--- node_rest/main.js
+++ node_rest/main.js
@@ -1,12 +1,17 @@
 const customExpress = require("./config/customExpress");
 const connection = require("./infra/connection");
+const Tables = require("./infra/tables");
 
-connection.connect((error) => {
-  if (error) {
-    console.log("error");
+connection.connect((err) => {
+  if (err) {
+    console.log("err");
   } else {
-    console.log("Database connection established!");
+    console.log("Database connection established");
+
+    Tables.init(connection);
+
     const app = customExpress();
+
     app.listen(3000, () => console.log("Server running on port 3000"));
   }
 });
