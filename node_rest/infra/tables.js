222bee6baba6652f599830c47e9575f2814675e8
Arnaldo Neto <costanetoow@gmail.com>
2022-03-17 11:39:37 -0300

    (Descontinuado) Adicionando moment e criando um novo campo na tabela

--- node_rest/infra/tables.js
+++ node_rest/infra/tables.js
@@ -7,7 +7,7 @@ class Tables {
 
   createCharacter() {
     const sql =
-      "CREATE TABLE IF NOT EXISTS Characters (id int NOT NULL AUTO_INCREMENT, name varchar(50) NOT NULL, race varchar(20), gender varchar(10), PRIMARY KEY (id))";
+      "CREATE TABLE IF NOT EXISTS characters (id int NOT NULL AUTO_INCREMENT, name varchar(50) NOT NULL, race varchar(20), gender varchar(10), charCreated datetime NOT NULL, date datetime NOT NULL, PRIMARY KEY (id))";
 
     this.connection.query(sql, (err) => {
       if (err) {
--- node_rest/models/character.js
+++ node_rest/models/character.js
@@ -1,10 +1,14 @@
+const moment = require("moment");
 const connection = require("../infra/connection");
 
 class Character {
   add(character) {
+    const charCreated = moment().format("YYYY-MM-DD HH:mm:ss");
+    const date = moment(character.date).format("YYYY-MM-DD HH:mm:ss");
+    const charDate = { ...character, charCreated, date };
     const sql = "INSERT INTO characters SET ?";
 
-    connection.query(sql, character, (err, result) => {
+    connection.query(sql, charDate, (err, result) => {
       if (err) {
         console.log(err);
       } else {
