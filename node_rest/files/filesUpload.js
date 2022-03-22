ae42688f73a8b165bad07a9ccc51144766124eb6
Arnaldo Neto <costanetoow@gmail.com>
2022-03-22 10:46:46 -0300

    (Descontinuado) Realizando stream de arquivos

--- node_rest/files/filesUpload.js
+++ node_rest/files/filesUpload.js
@@ -1,10 +1,7 @@
 const fs = require("fs");
 
-fs.readFile("./assets/goku.png", (error, buffer) => {
-  console.log("The image was buffered");
-
-  fs.writeFile("./assets/goku2.png", buffer, (error) => {
-    console.log("The image was wireted");
-  });
-});
-
+fs.createReadStream("./assets/goku.png").pipe(
+  fs.createWriteStream("./assets/goku-stream.png").on("finish", () => {
+    console.log("Image was writed");
+  })
+);
