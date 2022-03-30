a4bdd8c1a9c2c510ae5af29f77ac1de1ad307f1c
Arnaldo Neto <costanetoow@gmail.com>
2022-03-30 17:26:10 -0300

    Stack Overflow

---
+++ javascript/algoritmos_js/search.js
@@ -0,0 +1,23 @@
+const booksList = require("./orderedArray");
+
+function search(array, from, to, searchValue) {
+  console.log("from, to", from, to);
+  const middle = Math.floor((from + to) / 2);
+  const actual = array[middle];
+
+  if (searchValue === actual.price) {
+    return middle;
+  }
+
+  if (searchValue < actual.price) {
+    return search(array, from, middle - 1, searchValue);
+  }
+
+  if (searchValue > actual.price) {
+    return search(array, middle + 1, to, searchValue);
+  }
+}
+
+console.log(search(booksList, 0, booksList.length - 1, 20));
+
+// console.log(search(booksList, 0, booksList.length - 1, 60)) // Stack Overflow
