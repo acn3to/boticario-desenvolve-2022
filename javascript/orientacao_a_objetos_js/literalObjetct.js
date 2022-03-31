cc03abe85cca986c46ba854b6338e4e37a6fc28b
Arnaldo Neto <costanetoow@gmail.com>
2022-03-31 07:04:33 -0300

    Métodos call e apply

--- javascript/orientacao_a_objetos_js/literalObjetct.js
+++ javascript/orientacao_a_objetos_js/literalObjetct.js
@@ -16,15 +16,27 @@ const user = {
       `
     );
   },
+
+  showMessage(name, email) {
+    console.log(`User: ${name}, email: ${email}`);
+  },
+
+  callFunction: function (fn) {
+    fn.apply(user, [this.name, this.email]);
+  },
 };
 
 // user.showInfo();
 
-const show = function () {
-  console.log(this.name);
-};
+// const show = function () {
+//   console.log(this.name);
+// };
 
-const showName = show.bind(user);
+// const showName = show.bind(user); // Bind: É usado para ligar(bind) o valor de this a um determinado contexto.
 
-showName();
+// showName();
 // show();
+
+// show.call(user); //Call: É possível atribuir um this diferente do contexto atual ao executar a função.
+
+user.callFunction(user.showMessage); // O método apply() funciona de forma semelhante ao call(), porém recebe a lista de argumentos em um array.
