44ca569970f5977fc3fde9f1a0af1b0845128419
Arnaldo Neto <costanetoow@gmail.com>
2022-03-31 22:52:20 -0300

    Getters e Setters

--- javascript/orientacao_a_objetos_js/User.js
+++ javascript/orientacao_a_objetos_js/User.js
@@ -12,24 +12,56 @@ export default class User {
     this.#active = active;
   }
 
-  #createObject() {
-    return {
-      name: this.#name,
-      email: this.#email,
-      bornDate: this.#bornDate,
-      role: this.#role,
-      active: this.#active,
-    };
+  get name() {
+    return this.#name;
+  }
+
+  get email() {
+    return this.#email;
+  }
+
+  get bornDate() {
+    return this.#bornDate;
+  }
+
+  get role() {
+    return this.#role;
+  }
+
+  get active() {
+    return this.#active;
+  }
+
+  set name(newName) {
+    if (newName === "") {
+      throw new Error("Invalid format");
+    }
+    this.#name = newName;
+  }
+
+  set email(newEmail) {
+    this.#email = newEmail;
+  }
+
+  set bornDate(newBornDate) {
+    this.#bornDate = newBornDate;
+  }
+
+  set role(newRole) {
+    this.#role = newRole;
+  }
+
+  set active(newActive) {
+    this.#active = newActive;
   }
 
   showInfo() {
-    const userObject = this.#createObject();
     return `
-    Name: ${userObject.name} 
-    Email: ${userObject.email}
-    Birth Date: ${userObject.bornDate}o
-    Role: ${userObject.role}
-    Active: ${userObject.active}
+    Name: ${this.name} 
+    Email: ${this.email}
+    Birth Date: ${this.bornDate}o
+    Role: ${this.role}
+    Active: ${this.active}
     `;
   }
 }
--- javascript/orientacao_a_objetos_js/index.js
+++ javascript/orientacao_a_objetos_js/index.js
@@ -2,9 +2,10 @@ import User from "./User.js";
 import Teacher from "./Teacher.js";
 import Admin from "./Admin.js";
 
-const newUser = new User("Arnaldo", "costa.neto@gmail.com", "06/05/1993");
-console.log(newUser.showInfo());
+// const newUser = new User("Arnaldo", "costa.neto@gmail.com", "06/05/1993");
+// console.log(newUser.showInfo());
 
 const newAdmin = new Admin("Paula", "paula@gmail.com", "14/01/1994");
+newAdmin.name = "";
+console.log(newAdmin.name);
 console.log(newAdmin.showInfo());
-console.log(newAdmin.adminName());
